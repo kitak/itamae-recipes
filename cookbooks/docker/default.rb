12276a61368f39f15f1b1fb0c4eeca7e1fb527ad
Keisuke KITA <kei.kita2501@gmail.com>
2015-06-23 15:01:21 +0900

    dockerのレシピを書いた

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,1 +1,9 @@
-package 'docker'
+execute 'yum update for install docker' do
+  command 'yum update -y'
+  not_if 'test -e /usr/bin/docker'
+end
+
+execute 'install docker' do
+  command 'curl -O -sSL https://get.docker.com/rpm/1.7.0/centos-6/RPMS/x86_64/docker-engine-1.7.0-1.el6.x86_64.rpm && yum localinstall -y --nogpgcheck docker-engine-1.7.0-1.el6.x86_64.rpm && rm docker-engine-1.7.0-1.el6.x86_64.rpm'
+  not_if 'test -e /usr/bin/docker'
+end
